d3d5ca64783fbd35d5e943e8a278ad7bcb57531d
EvaZelenskaya <81824448+EvaZelenskaya@users.noreply.github.com>
2022-10-22 09:27:45 +0300

    +

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -4,17 +4,23 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
-
-  constructor() { }
-
+  constructor() {}
+  element: any;
   form: FormGroup;
-  weeks = [{
-    type: 'm',
-    text: 'Пн',
-  },
+  name: string;
+  autore: string;
+  typeOfPerson: any;
+  week: any;
+  longTour: boolean;
+
+  weeks = [
+    {
+      type: 'm',
+      text: 'Пн',
+    },
     {
       type: 'tu',
       text: 'Вт',
@@ -38,45 +44,55 @@ export class FormComponent implements OnInit {
     {
       type: 'sun',
       text: 'Вс',
-    }
-
+    },
   ];
-    ngOnInit() {
-      localStorage.setItem("formdata", JSON.stringify(null));
+  ngOnInit() {
+    // localStorage.setItem('formdata', JSON.stringify(null));
+
+    this.element = JSON.parse(localStorage.getItem('formdata') || '{}');
+    this.name = this.element.name;
+    this.autore = this.element.autore;
+    this.typeOfPerson = this.element.typeOfPerson;
+    this.week = this.element.week;
+    this.longTour = this.element.longTour;
+
     this.form = new FormGroup({
-      name: new FormControl(''),
-      autore: new FormControl(''),
-      typeOfPerson: new FormControl('mec'),
-      week: new FormControl('f'),
-      longTour: new FormControl(false)
-    })
-      this.form.valueChanges.subscribe(() => {
-        console.log(this.form)
-      })
-      this.form.get('longTour')?.valueChanges.subscribe(e => {
-        if(e) {
-          this.form.get('name')?.setValidators([Validators.required, Validators.minLength(3)]);
-          this.form.get('autore')?.setValidators([Validators.required, Validators.minLength(3)]);
-          this.form.get('typeOfPerson')?.setValidators([Validators.required]);
-          this.form.get('week')?.setValidators([Validators.required]);
-          this.form.updateValueAndValidity();
-        } else {
-          this.form.get('name')?.setValidators([Validators.nullValidator]);
-          this.form.get('autore')?.setValidators([Validators.nullValidator]);
-          this.form.get('typeOfPerson')?.setValidators([Validators.nullValidator]);
-          this.form.get('week')?.setValidators([Validators.nullValidator]);
-          this.form.updateValueAndValidity();
-        }
-        this.form.controls['name'].updateValueAndValidity();
-        this.form.controls['autore'].updateValueAndValidity();
-        this.form.controls['typeOfPerson'].updateValueAndValidity();
-        this.form.controls['week'].updateValueAndValidity();
+      name: new FormControl(this.name),
+      autore: new FormControl(this.autore),
+      typeOfPerson: new FormControl(this.typeOfPerson),
+      week: new FormControl(this.week),
+      longTour: new FormControl(this.longTour),
+    });
 
-      })
-
+    this.form.valueChanges.subscribe(() => {
+      console.log(this.form);
+    });
+    this.form.get('longTour')?.valueChanges.subscribe((e) => {
+      if (e) {
+        this.form
+          .get('name')
+          ?.setValidators([Validators.required, Validators.minLength(3)]);
+        this.form
+          .get('autore')
+          ?.setValidators([Validators.required, Validators.minLength(3)]);
+        this.form.get('typeOfPerson')?.setValidators([Validators.required]);
+        this.form.get('week')?.setValidators([Validators.required]);
+      } else {
+        this.form.get('name')?.setValidators([Validators.nullValidator]);
+        this.form.get('autore')?.setValidators([Validators.nullValidator]);
+        this.form
+          .get('typeOfPerson')
+          ?.setValidators([Validators.nullValidator]);
+        this.form.get('week')?.setValidators([Validators.nullValidator]);
+      }
+      this.form.controls['name'].updateValueAndValidity();
+      this.form.controls['autore'].updateValueAndValidity();
+      this.form.controls['typeOfPerson'].updateValueAndValidity();
+      this.form.controls['week'].updateValueAndValidity();
+    });
   }
 
   onSubmit() {
-    localStorage.setItem("formdata", JSON.stringify(this.form.value));
+    localStorage.setItem('formdata', JSON.stringify(this.form.value));
   }
 }
